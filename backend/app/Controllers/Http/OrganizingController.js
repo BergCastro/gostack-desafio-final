b7651587919627ada086ee92bb74f7cd5de4ab84
Lindemberg <lindembergncastro@outlook.com>
2019-10-25 14:30:42 -0300

    new mobile

--- backend/app/Controllers/Http/OrganizingController.js
+++ backend/app/Controllers/Http/OrganizingController.js
@@ -1,12 +1,15 @@
 'use strict'
+
 const Meetup = use('App/Models/Meetup')
 class OrganizingController {
   async index ({ auth }) {
     const meetups = await Meetup.query()
       .where('user_id', auth.user.id)
+      .andWhere('date', '>=', new Date())
       .with('file')
       .orderBy('date', 'desc')
       .fetch()
+
     return meetups
   }
 }
